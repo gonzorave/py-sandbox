87e6823ec291f42a2b789a2e07fc4a67ba74945e
dariaburkevich <46240655+gonzorave@users.noreply.github.com>
2025-11-10 23:49:47 +0300

    Update: added decorator example

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# This is a basic sample.
-
+""""
+This is a basic sample.
 print('Hello, Python!')
+"""
+
+# Decorator example
+def logger(func):
+    def wrapper(*args, **kwargs):
+        print('Calling:', func.__name__)
+        return func(*args, **kwargs)
+    return wrapper
+
+@logger
+def greeting():
+    print('I see ya!')
+
+@logger
+def farewell():
+    print('Goodbye!')
+
+# Сalls
+greeting()
+farewell()
